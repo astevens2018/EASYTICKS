769fbd26b0e8ea4761c33f0e02a3a863d0dfc0d8
Lui Kohl <lui.kohl@gmail.com>
2018-05-28 16:43:08 +0200

    routes are done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'pages#home'
-  # Need to add resources
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tickets
+  resource :requests, only: [:create, :destroy]
+  resource :reports, only: [:create, :show]
+
+  get '/dashboard', to: 'users#dashboard'
+
 end
+
