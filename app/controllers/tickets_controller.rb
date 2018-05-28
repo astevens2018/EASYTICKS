2ee09f3bed9741b74543dc48ab727d1403516f69
Lui Kohl <lui.kohl@gmail.com>
2018-05-28 17:16:29 +0200

    added tickets controller and users controller as well as views

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,22 @@
+class TicketsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+end
+
