b8f757ccad0a2f387eff64e811d6e072bd453215
Alex Stevens <astev2018@gmail.com>
2018-05-28 18:15:45 +0200

    frontend

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,22 +1,44 @@
 class TicketsController < ApplicationController
 
   def index
+    @tickets= Ticket.all
   end
 
   def show
+    @ticket = Ticket.find(params[:id])
   end
 
   def new
+    @ticket = Ticket.new
   end
 
   def create
+    @ticket = Ticket.new(host_params)
+    @ticket.save
+    redirect_to tickets_path(@ticket.id)
   end
 
   def edit
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def update
+    @ticket = Ticket.find(params[:id])
+    @ticket.update(host_params)
   end
 
   def destroy
+    @ticket = Ticket.find[params(:id)]
+    @ticket.destroy
+    redirect_to tickets_path
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:user_id, :date, :time, :departing_city, :arrival_city, :price, :booking_reference)
   end
 
 end
+
 
