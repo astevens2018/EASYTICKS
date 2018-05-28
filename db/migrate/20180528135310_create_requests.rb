5a2bf11dc7a8b4e250b9a13f7657004293a23259
Uma Patel <umabpatel@gmail.com>
2018-05-28 16:46:20 +0200

    more model

--- db/migrate/20180528135310_create_requests.rb
+++ db/migrate/20180528135310_create_requests.rb
@@ -1,7 +1,7 @@
 class CreateRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :requests do |t|
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: true, index: true
       t.text :preference
 
       t.timestamps
