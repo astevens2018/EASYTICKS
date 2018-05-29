3541e66b32016578593451fc9c804295cf29129e
Lui Kohl <lui.kohl@gmail.com>
2018-05-29 15:26:06 +0200

    made index dynamic

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+
 u1 = User.create!(
   email: "alex@gmail.com",
   password: "Rubyonrails",
