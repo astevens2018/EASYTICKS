eb80a6d43a539d9a260ad908656e4f2fd2ff86ca
Alex Stevens <astev2018@gmail.com>
2018-05-29 12:31:15 +0200

    frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+u1 = User.create!(
+  email: "alex@gmail.com",
+  password: "Rubyonrails",
+  firstname: "Alexander",
+  lastname: "Stevens",
+  bankaccount: "BE123456789",
+  phone: 324744448)
+
+u2 = User.create!(
+  email: "uma@gmail.com",
+  password: "Bitchonrails",
+  firstname: "Uma",
+  lastname: "Patel",
+  bankaccount: "AU123456789",
+  phone: 494744448)
+
 t1 = Ticket.create!(
-  buyer_id:
-  seller_id:
-  date: "15/06/2018"
-  time: "6:00PM"
-  departing_city: "Brussels"
-  arrival_city: "London"
-  price: "69€"
-  booking_reference: "TW6GZ2"
+  seller_id: 1,
+  date: DateTime.new(2018,5,27,18,30,0),
+  departing_city: "Brussels",
+  arrival_city: "London",
+  price: 69,
+  booking_reference: "TW6GZ2")
+
+t2 = Ticket.create!(
+  seller_id: 2,
+  date: DateTime.new(2018,6,27,7,0,0),
+  departing_city: "Paris",
+  arrival_city: "London",
+  price: 50,
+  booking_reference: "AS7FZ2")
+
 
