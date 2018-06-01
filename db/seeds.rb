b7f91c61206716447de3e5757906a316fddb25b4
Alex Stevens <astev2018@gmail.com>
2018-06-01 15:13:52 +0200

    love

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Cleaning database...'
+Ticket.destroy_all
 User.destroy_all
 
 u1 = User.create!(
