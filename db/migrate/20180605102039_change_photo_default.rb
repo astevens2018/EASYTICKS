93ced4d86e00d3b01fc102b20dae758ae9842724
Uma Patel <umabpatel@gmail.com>
2018-06-05 12:53:33 +0200

    table photo done

--- db/migrate/20180605102039_change_photo_default.rb
+++
@@ -1,5 +0,0 @@
-class ChangePhotoDefault < ActiveRecord::Migration[5.2]
-  def change
-  change_column_null :users, :photo, 'http://static.tvtropes.org/pmwiki/pub/images/320px-thomasandgordon26_1713.png'
-  end
-end
